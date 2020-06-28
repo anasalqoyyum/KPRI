4d3d1707536ae6c176335515c3259be195bd8d28
darknowns <doublezero13@gmail.com>
2020-06-28 20:42:00 +0700

    Fix

--- application/views/import.php
+++ application/views/import.php
@@ -1,25 +1,72 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Import Excel CodeIgniter</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <?php $this->load->view('template/head.php'); ?>
 </head>
-<body>
 
-<div class="container" style="margin-top: 100px">
-    <div class="row">
-        <div class="col-md-8 offset-2">
-            <?php echo $this->session->flashdata('notif') ?>
-            <form method="POST" action="<?php echo base_url() ?>import/upload" enctype="multipart/form-data">
-              <div class="form-group">
-                <label for="exampleInputEmail1">UNGGAH FILE EXCEL</label>
-                <input type="file" name="userfile" class="form-control">
-              </div>
+<body class="hold-transition sidebar-mini layout-fixed">
 
-              <button type="submit" class="btn btn-success">UPLOAD</button>
-            </form>
-        </div>
+  <?php $this->load->view('template/navbar.php'); ?>
+  <?php $this->load->view('template/sidebar.php'); ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Import Excel</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <?php $this->load->view('template/breadcrumb.php'); ?>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-</div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Main row -->
+        <div class="row">
+          <div class="col-4"></div>
+          <div class="col-4">
+            <div class="card">
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-8 offset-2">
+                    <?php echo $this->session->flashdata('notif') ?>
+                    <form method="POST" action="<?php echo base_url() ?>import/upload" enctype="multipart/form-data">
+                      <div class="form-group">
+                        <input name="userfile" type="file" class="custom-file-input <?php echo form_error('datask') ? 'is-invalid' : '' ?>" id="exampleInputFile"></input>
+                        <label class="custom-file-label align-center" for="exampleInputFile">Masukkan Excel</label>
+                      </div>
+                      <button type="submit" class="btn btn-success">Import</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.Left col -->
+  </div>
+  <!-- /.row (main row) -->
+  </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+  </div>
+
+  <?php $this->load->view('template/footer.php'); ?>
+  <?php $this->load->view('template/js.php'); ?>
 </body>
+
 </html>
